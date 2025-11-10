7e9bfba04949ad1857df98b39fd9e6d91e21fa4d
Ivailoboyadzhiev <ivicataboyadzhiev@gmail.com>
2025-11-10 17:51:09 +0200

    Initial projects structure

---
+++ MyFitnessJourneyBackend/src/test/java/com/myfitnessjourney/myfitnessjourney/MyFitnessJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyFitnessJourney.MyFitnessJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitnessJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ MyFitnessJourneyFrontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div style={{textAlign: 'center', padding: '2rem'}}>
+      <h1>Welcome to MyFitnessJourney (Vite + React + TypeScript)</h1>
+      <p>Frontend is running successfully 🚀</p>
+    </div>
+  )
+}
+
+export default App
